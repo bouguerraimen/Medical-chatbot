2220a21e50a0ee9041f7feb7cbdb2843bf6d0c62
Imen Bouguerra <imenbouguerra253@gmail.com>
2025-10-13 23:27:28 +0200

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="generative_ai_project",
+    version='0.0.0',
+    author='Bouguerra Imen',
+    email="imenbouguerra253@gamil.com",
+    packages=find_packages(),
+    install_requires=[]
+)
